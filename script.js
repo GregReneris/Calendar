9e53ce260e0ba3ccba9c875d6e8f1bd625189212
Gregory Reneris <greg@reneris.com>
2019-12-10 19:54:38 -0800

    fixed a local storage error. added colors. Cleared up some lingering borders.

--- script.js
+++ script.js
@@ -1,15 +1,14 @@
 
 var hours = ["09:00","10:00","11:00","12:00","13:00","14:00","15:00","16:00","17:00"];
 
-var schedule = null;
-var schedule = localStorage.getItem("schedule");
+var schedule = ["Cheers mate","Party On","Hello","","","","everything","",""];
+var schedule =  JSON.parse(localStorage.getItem("schedule"));
 
+console.log( "START: " + schedule );
+console.log( "START: " + typeof schedule );
 
-if (schedule != null){
-    schedule = JSON.parse(schedule);
-}   
-else
-{
+if ( !(schedule instanceof Array) ){
+    console.log( "Reset schedule" );
     schedule=[];
     hours.forEach( function(e){ schedule.push(""); } );
 }
